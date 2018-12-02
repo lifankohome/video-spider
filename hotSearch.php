0992828beb49471c7e5e9b1749d15b3460a23d4a
lifankohome <lifankohome@163.com>
2018-12-02 17:35:16 +0800

    Modify TITLE Of hotsearch

--- hotSearch.php
+++ hotSearch.php
@@ -38,7 +38,7 @@ if (empty($_GET['max'])) { //显示的关键词数量，默认最多显示999个
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>电影 - 影视爬虫</title>
+    <title>影视排行榜 - 影视爬虫</title>
     <?php
     echo Common::SEO;
     ?>
