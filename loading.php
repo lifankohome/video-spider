4296ccfcdd63206b9dbc70e9659f961cd567e64e
lifankohome <lifankohome@163.com>
2019-02-22 19:26:59 +0800

    To Common Version

--- loading.php
+++ loading.php
@@ -35,7 +35,7 @@
     <h2 style="margin: 1pc 0 0"><img src="img/logo.png" style="height: 70px;margin-bottom: -27px"> 影视爬虫v3.9<span
             style="font-size: 12px">（2019/2/21 11:46更新）</span></h2>
     <p>如果觉得本站好用，请将 <strong style="color: #467cff">影视爬虫</strong> 推荐给您的朋友！</p>
-    <p style="margin-top: 100px">科学上网海外高速节点，10美刀12个月，每月1000GB流量 ↓<br><a href="https://www.vultr.com/?ref=7892094" target="_blank"><img
+    <p style="margin-top: 100px">科学上网海外高速节点，新用户充值10美刀送50美刀，5刀每月1000GB流量 ↓<br><a href="https://www.vultr.com/?ref=7892094" target="_blank"><img
                 src="img/vultr.png" style="background-color: #1669ba;padding: 10px;width: 50%;margin-top: 5px"></a></p>
     <div style="position: fixed;bottom: 0;width: 100%;margin: 0 auto">
         <p>科学上网15天仅需2元，800MB流量：<a href="http://jiasd.us/4297" target="_blank"><img
