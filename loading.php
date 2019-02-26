c77e9b13321a2cf691f513e82a2ac12c0cf413c5
lifankohome <lifankohome@163.com>
2019-02-26 16:47:06 +0800

    Add festival-img show

--- loading.php
+++ loading.php
@@ -1,3 +1,33 @@
+<?php
+$festival = file_get_contents('http://ali.lifanko.cn/festival2img/index.php');
+
+if (!empty($festival)) {
+    $src = explode(':http', $festival);
+    $lightness = mb_substr($src[0], 0, 1);
+    $src = 'http' . $src[1];
+    if ($lightness == '0') {
+        $css = "    text-shadow: 0px 0px 5px black;
+            color: white;";
+    } else if ($lightness == '1') {
+        $css = "    text-shadow: 0px 0px 5px white;
+        color: black;";
+    } else {
+        $css = '    ';
+    }
+
+    $css .= "
+            background: url($src) no-repeat fixed center;
+            background-size: 100% 100%;
+        }
+
+        #container {
+            opacity: 0;
+            transition: all 0.8s 0s;
+        }";
+} else {
+    $css = '';
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,22 +42,9 @@
             text-align: center;
             height: 100%;
             overflow-y: hidden;
-
         <?php
-            $time = '190219';
-            if(date('ymd', time()) == $time){
-                echo "background: url(\"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550505358768&di=e6d0c7fe3b6f14ca1d07cbe07b314e0b&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F3%2F589d1e4442f6a.jpg\") no-repeat fixed center;
-                background-size: 100% 100%;";
-            } ?>
-        }
-
-        #container {
-        <?php
-        if(date('ymd', time()) == $time){
-            echo "opacity: 0;";
-        }
-        ?> transition: all 0.8s 0s;
-        }
+        if(!empty($css))echo $css;
+        ?>
     </style>
 </head>
 <body>
@@ -35,7 +52,8 @@
     <h2 style="margin: 1pc 0 0"><img src="img/logo.png" style="height: 70px;margin-bottom: -27px"> 影视爬虫v3.9<span
             style="font-size: 12px">（2019/2/21 11:46更新）</span></h2>
     <p>如果觉得本站好用，请将 <strong style="color: #467cff">影视爬虫</strong> 推荐给您的朋友！</p>
-    <p style="margin-top: 100px">科学上网海外高速节点，新用户充值10美刀送50美刀，5刀每月1000GB流量 ↓<br><a href="https://www.vultr.com/?ref=7892094" target="_blank"><img
+    <p style="margin-top: 100px">科学上网海外高速节点，新用户充值10美刀送50美刀，5刀每月1000GB流量 ↓<br><a
+            href="https://www.vultr.com/?ref=7892094" target="_blank"><img
                 src="img/vultr.png" style="background-color: #1669ba;padding: 10px;width: 50%;margin-top: 5px"></a></p>
     <div style="position: fixed;bottom: 0;width: 100%;margin: 0 auto">
         <p>科学上网15天仅需2元，800MB流量：<a href="http://jiasd.us/4297" target="_blank"><img
