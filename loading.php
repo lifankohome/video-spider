8092372f39493b871a1731cf358d975aca4f360d
lifankohome <lifankohome@163.com>
2019-07-03 17:50:48 +0800

    Add Festival Tip

--- loading.php
+++ loading.php
@@ -2,34 +2,13 @@
 include_once 'Cinema/Maxim.php';
 use Cinema\Maxim;
 
-$festival = file_get_contents('https://www.lifanko.cn/festival2img/index.php?day=' . date('Ymd', time()));
+$festival = file_get_contents('https://www.lifanko.cn/festival2img/api.php');
 
-if (!empty($festival)) {
-    $src = explode(':http', $festival);
-    $lightness = mb_substr($src[0], 0, 1);
-    $src = 'http' . $src[1];
-    if ($lightness == '0') {
-        $css = "    text-shadow: 0px 0px 5px black;
-            color: white;";
-    } else if ($lightness == '1') {
-        $css = "    text-shadow: 0px 0px 5px white;
-            color: black;";
-    } else {
-        $css = '    ';
-    }
-
-    $css .= "
-            background: url($src) no-repeat fixed center;
-            background-size: 100% 100%;
-        }
-
-        #container {
-            opacity: 0;
-            transition: all 0.8s 0s;
-        }
-";
+$festival = mb_substr($festival, mb_strpos($festival, ':') + 1);
+if ($festival != 'No Festival') {
+    $festival = '（' . $festival . '）';
 } else {
-    $css = '';
+    $festival = '';
 }
 ?>
 <!DOCTYPE html>
@@ -46,26 +25,30 @@ if (!empty($festival)) {
             text-align: center;
             height: 100%;
             overflow-y: hidden;
-        <?php
-        if(!empty($css)){
-        echo $css;
-        }else{
-        echo "}
-";
         }
-        ?>
     </style>
 </head>
 <body>
 <div id="container">
-    <h2 style="margin: 1pc 0 0"><img src="img/logo.png" style="height: 70px;margin-bottom: -27px"> 影视爬虫v4.0<span
-            style="font-size: 12px">（2019/6/30 17:27更新 已支持HTTPS）</span></h2>
-    <p>如果觉得本站好用，请将 <strong style="color: #467cff">影视爬虫</strong> 推荐给您的朋友 或
-        <span onmouseover="showDonate()" onmouseout="hideDonate()" style="color: #e22c1b;cursor: pointer;text-decoration: underline"
-              id="donateTip">给作者打赏~</span><img src="img/wechat.jpg" id="wechat" style="width: 24%;display: block;margin: -100pc auto 100px auto;"></p>
-    <div id="ad" style="width: 60%;margin: -20px auto 0 auto;font-size: 24px;"><h4 style="border-bottom: 1px #999 solid;">今日箴言</h4><p style="font-size: 15px"><?php echo Maxim::get(); ?></p></div>
+    <h2 style="margin: 1pc 0 0">
+        <img src="img/logo.png" style="height: 70px;margin-bottom: -27px"> 影视爬虫v4.0
+        <span style="font-size: 12px">(2019/6/30 17:27更新 已支持HTTPS)</span>
+    </h2>
+    <p>
+        如果觉得本站好用，请将 <strong style="color: #467cff">影视爬虫</strong> 推荐给您的朋友 或
+        <span onmouseover="showDonate()" onmouseout="hideDonate()"
+              style="color: #e22c1b;cursor: pointer;text-decoration: underline"
+              id="donateTip">给作者打赏~
+        </span>
+        <img src="img/wechat.jpg" id="wechat" style="width: 24%;display: block;margin: -100pc auto 100px auto;">
+    </p>
+    <div id="ad" style="width: 60%;margin: -20px auto 0 auto;font-size: 24px;">
+        <h4 style="border-bottom: 1px #999 solid;">今日<?php echo $festival; ?>箴言</h4>
+        <p style="font-size: 15px"><?php echo Maxim::get(); ?></p></div>
     <div style="position: fixed;bottom: 0;width: 100%;margin: 0 auto">
-        <p style="margin-top: 2%">电影请点击左上方“立即播放”、电视剧选集后即可播放<br>播放不了请尝试切换下方的解析器 或 反馈站长：lifankohome@163.com</p>
+        <p style="margin-top: 2%">
+            电影请点击左上方“立即播放”、电视剧选集后即可播放<br>播放不了请尝试切换下方的解析器 或 反馈站长：lifankohome@163.com
+        </p>
     </div>
 </div>
 <script>
