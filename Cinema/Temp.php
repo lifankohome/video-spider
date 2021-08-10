c9c86d43b4de569815a59a23e79ef6c9bfcf1b4f
lifankohome <lifankohome@163.com>
2021-08-10 18:31:11 +0800

    Make Temp Engine Support Redis

--- Cinema/Temp.php
+++ Cinema/Temp.php
@@ -8,72 +8,135 @@ namespace Cinema;
  */
 class Temp
 {
-    const path = './Cinema/temp/';
+    private array $_config = [
+        'redis' => [
+            'host' => '127.0.0.1',
+            'port' => 6379,
+            'pass' => '',
+            'expire' => 86400 / 2
+        ],
+        'file' => [
+            'path' => './Cinema/temp/',
+            'ext' => 'json',
+            'expire' => 86400
+        ]
+    ];
 
-    /**
-     * @var int
-     */
-    private $time;
-    private $key;
+    // Judge engine by this variable
+    private ?\Redis $_redis = null;
+
+    private string $_key;
+    private string $_prefix = 'Video_Temp_';
 
     /**
      * Temp constructor.
+     * @param $key
+     * @param string $engine
      */
-    public function __construct($key)
+    public function __construct($key, $engine = 'redis')
     {
-        if (!is_dir(self::path)) {
-            mkdir(self::path, 0777, true);
+        $this->_key = $key;
+
+        if ($engine == 'redis') {
+            if (!extension_loaded('redis')) {
+                die('No Redis Extension');
+            }
+            $this->_redis = new \Redis();
+            $this->_redis->connect($this->_config['redis']['host'], $this->_config['redis']['port']);
+            if ('' != $this->_config['redis']['pass']) {
+                $this->_redis->auth($this->_config['redis']['pass']);
+            }
+        } else {
+            if (!is_dir($this->_config[$engine]['path'])) {
+                mkdir($this->_config[$engine]['path'], 0777, true);
+            }
         }
-
-        $this->key = $key;
-        $this->time = time();
-    }
-
-    public function filename()
-    {
-        return self::path . $this->key . '.json';
     }
 
     /**
+     * Get Key Temped
+     * @return bool|mixed
+     */
+    private function key()
+    {
+        if ($this->_redis) {
+            return $this->_prefix . $this->_key;
+        } else {
+            return $this->_config['file']['path'] . $this->_prefix . $this->_key . '.' . $this->_config['file']['ext'];
+        }
+    }
+
+    /**
+     * Get object Temped
      * @return bool|mixed
      */
     public function get()
     {
-        if (is_file($this->filename())) {
-            $res = file_get_contents($this->filename());
-            if (empty($res)) {
-                $this->del();
-
-                return false;
-            }
-
-            $res = json_decode($res, true);
-            if ($res['keep'] > $this->time) {
+        if ($this->_redis) {
+            $obj = $this->_redis->get($this->key());
+            if ($obj) {
+                $res = json_decode($obj, true);
                 return $res['obj'];
             } else {
-                $this->del();
+                return false;
             }
+        } else {
+            if (is_file($this->key())) {
+                $obj = file_get_contents($this->key());
+                if (empty($obj)) {
+                    $this->del();
+                    return false;
+                }
+
+                $res = json_decode($obj, true);
+                if ($res['keep'] > time()) {
+                    return $res['obj'];
+                } else {
+                    $this->del();
+                }
+            }
+            return false;
         }
-        return false;
     }
 
     /**
+     * Save object
      * @param $obj
-     * @param int $keep
+     * @param bool|int $expire
      * @return false|int number of written
      */
-    public function save($obj, $keep = 86400)
+    public function save($obj, $expire = false)
     {
-        $res = ['keep' => $this->time + $keep, 'obj' => $obj];
+        if ($expire === false) {
+            $expire = $this->_config[$this->_redis ? 'redis' : 'file']['expire'];
+        }
 
-        return file_put_contents($this->filename(), json_encode($res));
+        $res = json_encode(['keep' => time() + $expire, 'obj' => $obj]);
+        if ($this->_redis) {
+            return (boolean)$this->_redis->setex($this->key(), $expire, $res);
+        } else {
+            return (boolean)file_put_contents($this->key(), $res);
+        }
     }
 
     /**
+     * Delete specific key
      * @return bool success or fail
      */
     private function del()
     {
-        return unlink($this->filename());
+        if ($this->_redis) {
+            return (boolean)$this->_redis->del($this->key());
+        } else {
+            return unlink($this->key());
+        }
+    }
+
+    /**
+     * @param string $prefix
+     */
+    public function setPrefix(string $prefix): void
+    {
+        $this->_prefix = $prefix;
     }
 }
